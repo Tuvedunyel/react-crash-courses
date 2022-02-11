7d0830bda802c83b7c104f50cf6ee73e1db88a6d
Grégoire <72552360+Tuvedunyel@users.noreply.github.com>
2022-02-11 13:20:41 +0100

    Modal start

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,10 +1,17 @@
 function Todo(props) {
+  function handleDelete() {
+    console.log("Clicked!");
+    console.log(props.title);
+  }
+
   return (
     <>
       <div className='card'>
         <h2>{props.title}</h2>
         <div className='actions'>
-          <button className='btn'>Delete</button>
+          <button className='btn' onClick={handleDelete}>
+            Delete
+          </button>
         </div>
       </div>
     </>
