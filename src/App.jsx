9cb7c14d5654848700079357aeed1fc6d3bf8c54
Grégoire <72552360+Tuvedunyel@users.noreply.github.com>
2022-02-15 11:07:13 +0100

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
-import Todo from "./components/Todo";
 function App() {
   return (
     <>
-      <h1>My Todos</h1>
-      <Todo title='Learn React' />
-      <Todo title='Master React' />
-      <Todo title='Explore the full React Course' />
+      <h1>Routing</h1>
     </>
   );
 }
